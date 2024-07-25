abac8cdc305d2872066018c68b8c43655e851aa8
cub <hellomynameiscub@gmail.com>
2024-07-25 10:11:28 +0200

    default formatting

--- src/routes/api/airports/+server.js
+++ src/routes/api/airports/+server.js
@@ -1,24 +1,29 @@
 export async function GET({ fetch, url }) {
-  const query = await url.searchParams.get('query');
+  const query = await url.searchParams.get("query");
 
-  const res = await fetch("https://www.google.com/_/TravelFrontendUi/data/batchexecute", {
-    "headers": {
-      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-    },
-    "body": `f.req=${encodeURIComponent(`[[["H028ib","[\\"${query}\\",[1,2,3,5],null,[2],1]",null,"generic"]]]`)}`,
-    "method": "POST"
-  });
-  
+  const res = await fetch(
+    "https://www.google.com/_/TravelFrontendUi/data/batchexecute",
+    {
+      headers: {
+        "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+      body: `f.req=${encodeURIComponent(
+        `[[["H028ib","[\\"${query}\\",[1,2,3,5],null,[2],1]",null,"generic"]]]`
+      )}`,
+      method: "POST",
+    }
+  );
+
   const text = await res.text();
-  
+
   try {
     const json = JSON.parse(text.slice(6));
     const data = JSON.parse(json[0][2])[0];
-  
+
     if (!data) return new Response(JSON.stringify({}));
-  
+
     const airports = {};
-  
+
     for (const item of data) {
       if (item[0][0] === 1) {
         airports[item[0][5]] = item[0][1];
@@ -30,8 +35,8 @@ export async function GET({ fetch, url }) {
         }
       }
     }
-  
-    return new Response(JSON.stringify(airports))
+
+    return new Response(JSON.stringify(airports));
   } catch (e) {
     return new Response(JSON.stringify({}));
   }
